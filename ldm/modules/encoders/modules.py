b5a769080e42e5907ff750bf09edcc455eee5983
Alex Birch <Birch-san@users.noreply.github.com>
2022-10-01 00:55:10 +0100

    simplify

--- ldm/modules/encoders/modules.py
+++ ldm/modules/encoders/modules.py
@@ -1,16 +1,14 @@
 import torch
-import torch.nn as nn
-from transformers.models.clip.modeling_clip import CLIPTextTransformer
+from transformers.models.clip.modeling_clip import CLIPTextEmbeddings
 from transformers.models.clip.configuration_clip import CLIPTextConfig
 
 from ldm.modules.embedding_manager import EmbeddingManager
 
-class FrozenCLIPEmbedder(nn.Module):
+class FrozenCLIPEmbedder():
     """Uses the CLIP transformer encoder for text (from Hugging Face)"""
     def __init__(self, version="openai/clip-vit-large-patch14"):
-        super().__init__()
         config = CLIPTextConfig.from_pretrained(version)
-        self.text_model = CLIPTextTransformer(config)
+        self.embeddings = CLIPTextEmbeddings(config).to('mps')
 
         def embedding_forward(
             self,
@@ -19,8 +17,7 @@ class FrozenCLIPEmbedder(nn.Module):
             inputs_embeds = None,
             embedding_manager = None,
         ) -> torch.Tensor:
-            if position_ids is None:
-                position_ids = self.position_ids[:, :77]
+            position_ids = torch.arange(0, 77, device='mps').unsqueeze(0)
 
             if inputs_embeds is None:
                 inputs_embeds = self.token_embedding(input_ids)
@@ -30,18 +27,7 @@ class FrozenCLIPEmbedder(nn.Module):
 
             self.position_embedding(position_ids)      
 
-        self.text_model.embeddings.forward = embedding_forward.__get__(self.text_model.embeddings)
-
-
-        def text_encoder_forward(
-            self,
-            input_ids = None,
-            position_ids = None,
-            embedding_manager = None,
-        ):
-            self.embeddings(input_ids=input_ids, position_ids=position_ids, embedding_manager=embedding_manager)
-
-        self.text_model.forward = text_encoder_forward.__get__(self.text_model)
+        self.embeddings.forward = embedding_forward.__get__(self.embeddings)
 
     def repro(self, embedding_manager: EmbeddingManager):
-        self.text_model(input_ids=torch.cat([torch.tensor([49406], device='mps'), torch.tensor([49407], device='mps').expand(76)]).unsqueeze(0), embedding_manager=embedding_manager)
+        self.embeddings(input_ids=torch.cat([torch.tensor([49406], device='mps'), torch.tensor([49407], device='mps').expand(76)]).unsqueeze(0), embedding_manager=embedding_manager)
