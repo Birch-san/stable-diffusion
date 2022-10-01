88e938d7689fa1b46185e596ea478ac0e74b5bdf
Alex Birch <Birch-san@users.noreply.github.com>
2022-10-01 01:19:49 +0100

    simplify

--- scripts/txt2img_fork.py
+++ scripts/txt2img_fork.py
@@ -1,26 +1,11 @@
-# from torch import save, load, tensor
 # from torch.nn import ParameterDict
-from torch import tensor, load, cat
+from torch import tensor, load, cat#, save
 from torch.nn import Embedding
-from ldm.modules.embedding_manager import EmbeddingManager
 
 def main():
     device='mps'
     token_embedding = Embedding(49408, 768, device=device)
-
-    string_to_token_dict = {}
-
-    token = tensor(265, device=device)
-
-    string_to_token_dict['*'] = token
     
-    ckpt = load('/Users/birch/git/stable-diffusion/logs/2022-09-20T01-49-11_fumo/checkpoints/embeddings.pt', map_location='cpu')
-
-    # Handle .pt textual inversion files
-    # self.string_to_token_dict = { '*': tensor(265, device='cpu') }
-    if 'string_to_token' in ckpt:
-        string_to_token_dict = ckpt["string_to_token"]
-
     # filename = 'test.pt'
     # save({
     #     'string_to_token': {
@@ -30,10 +15,13 @@ def main():
     # }, filename)
     # x = load(filename, map_location='cpu')
 
-    # embedding_manager.load('/Users/birch/git/stable-diffusion/logs/2022-09-20T01-49-11_fumo/checkpoints/embeddings.pt')
-    # embedding_manager.load(filename)
+    # ckpt = load(filename)
+    ckpt = load('/Users/birch/git/stable-diffusion/logs/2022-09-20T01-49-11_fumo/checkpoints/embeddings.pt', map_location='cpu')
 
-    # embedding_manager.repro()
+    # Handle .pt textual inversion files
+    # self.string_to_token_dict = { '*': tensor(265, device='cpu') }
+    assert 'string_to_token' in ckpt
+    string_to_token_dict = ckpt["string_to_token"]
 
     tokenized_text = cat([tensor([49406], device=device), tensor([49407], device=device).expand(76)]).unsqueeze(0)
     token_embedding(tokenized_text)
