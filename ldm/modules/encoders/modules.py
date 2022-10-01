435c6b1c337c943f00dbdf3fe1c12f72ea6e404f
Alex Birch <Birch-san@users.noreply.github.com>
2022-10-01 01:03:19 +0100

    simplify

--- ldm/modules/encoders/modules.py
+++ ldm/modules/encoders/modules.py
@@ -1,33 +1,22 @@
 import torch
-from transformers.models.clip.modeling_clip import CLIPTextEmbeddings
-from transformers.models.clip.configuration_clip import CLIPTextConfig
+from torch.nn import Embedding
 
 from ldm.modules.embedding_manager import EmbeddingManager
 
 class FrozenCLIPEmbedder():
     """Uses the CLIP transformer encoder for text (from Hugging Face)"""
-    def __init__(self, version="openai/clip-vit-large-patch14"):
-        config = CLIPTextConfig.from_pretrained(version)
-        self.embeddings = CLIPTextEmbeddings(config).to('mps')
+    def __init__(self):
+        self.token_embedding = Embedding(49408, 768, device='mps')
+        self.position_embedding = Embedding(77, 768, device='mps')
 
-        def embedding_forward(
-            self,
-            input_ids = None,
-            position_ids = None,
-            inputs_embeds = None,
-            embedding_manager = None,
-        ) -> torch.Tensor:
-            position_ids = torch.arange(0, 77, device='mps').unsqueeze(0)
+    def repro(
+        self,
+        embedding_manager: EmbeddingManager,
+    ) -> torch.Tensor:
+        input_ids=torch.cat([torch.tensor([49406], device='mps'), torch.tensor([49407], device='mps').expand(76)]).unsqueeze(0)
+        position_ids = torch.arange(0, 77, device='mps').unsqueeze(0)
 
-            if inputs_embeds is None:
-                inputs_embeds = self.token_embedding(input_ids)
+        inputs_embeds = self.token_embedding(input_ids)
+        inputs_embeds = embedding_manager(input_ids, inputs_embeds)
 
-            if embedding_manager is not None:
-                inputs_embeds = embedding_manager(input_ids, inputs_embeds)
-
-            self.position_embedding(position_ids)      
-
-        self.embeddings.forward = embedding_forward.__get__(self.embeddings)
-
-    def repro(self, embedding_manager: EmbeddingManager):
-        self.embeddings(input_ids=torch.cat([torch.tensor([49406], device='mps'), torch.tensor([49407], device='mps').expand(76)]).unsqueeze(0), embedding_manager=embedding_manager)
+        self.position_embedding(position_ids)
