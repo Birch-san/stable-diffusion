fd86ba0163b5e5c649400b993738cf3d927c61d6
Alex Birch <Birch-san@users.noreply.github.com>
2022-10-24 23:30:08 +0100

    time generation

--- scripts/coreml_play.py
+++ scripts/coreml_play.py
@@ -4,6 +4,7 @@ import torch
 from diffusers import StableDiffusionPipeline, KarrasVeScheduler
 # from diffusers import StableDiffusionPipeline, LMSDiscreteScheduler
 from PIL import Image
+import time
 
 kve = KarrasVeScheduler(
     sigma_max=14.6146,
@@ -18,19 +19,24 @@ kve = KarrasVeScheduler(
 # )
 
 pipe = StableDiffusionPipeline.from_pretrained("/Users/birch/git/stable-diffusion-v1-4", safety_checker=None)# torch_type=torch.float16, revision="fp16")
+
+tic = time.perf_counter()
 pipe = pipe.to("mps")
 
 prompt = "masterpiece character portrait of a blonde girl, full resolution, 4k, mizuryuu kei, akihiko. yoshida, Pixiv featured, baroque scenic, by artgerm, sylvain sarrailh, rossdraws, wlop, global illumination, vaporwave"
-generator = torch.Generator(device="cpu").manual_seed(1024)
+generator = torch.Generator(device="cpu").manual_seed(68673924)
 image: Image.Image = pipe(
 	prompt,
 	# guidance_scale=1.,
 	generator=generator,  
   # scheduler=lms,
   scheduler=kve,
-  num_inference_steps=30
+  # num_inference_steps=30
+  num_inference_steps=15
 ).images[0]
 
 sample_path="outputs/diffusers"
 base_count = len(os.listdir(sample_path))
 image.save(os.path.join(sample_path, f"{base_count:05}.png"))
+toc = time.perf_counter()
+print(f'in total, generated 1 image in {toc-tic} seconds')
